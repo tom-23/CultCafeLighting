ab5299f657aba2bb9eb4b83ba3dcc1d698c14396
Tom Butcher <thomasbutcher15@gmail.com>
2022-04-11 15:54:27 +0100

    Add profile file download

--- start_script.sh
+++ start_script.sh
@@ -1,5 +1,10 @@
 echo "Downloading fixtures..."
 
-echo "Starting QLCPlus..."
+echo "Downloading project file..."
+
+cd /home/tom
+wget -O cult.qxw "https://raw.githubusercontent.com/tom-23/CultCafeLighting/main/cult.qxw"
+
+echo "All updates complete! Starting QLCPlus..."
 
 xvfb-run qlcplus -n -p --web -d -o cult.qxw
